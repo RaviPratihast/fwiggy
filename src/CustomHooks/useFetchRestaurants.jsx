a5f187107859a0b593f3ca007515ad4ba8907e71
Ravi Pratihast <ravipratihast@gmail.com>
2025-01-07 16:52:48 +0530

    Feat: created custom hooks for API calls.

---
+++ src/CustomHooks/useFetchRestaurants.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+export const useFetchRestaurants = (url) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, error, loading };
+};
