0b2083095134821a5b25d8ac35a80f3fee6296c2
Ravi Pratihast <ravipratihast@gmail.com>
2025-01-11 22:04:53 +0530

    add: constant URLs that need to be called.

--- src/apiEndpoints/apiEndPoints.jsx
+++ src/apiEndpoints/apiEndPoints.jsx
@@ -1,6 +1,8 @@
 const RestaurantsURL =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
-const ImageURL =
-  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+const ImageURL = `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/`;
+const MenuURL = `https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.96340&lng=77.58550&restaurantId=`;
+const DishesPhotoURL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/";
 
-export { RestaurantsURL, ImageURL };
+export { RestaurantsURL, ImageURL, MenuURL, DishesPhotoURL };
