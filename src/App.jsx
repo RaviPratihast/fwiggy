1befd6bdde4a1388584c6beb64256a911d268555
Ravi Pratihast <ravipratihast@gmail.com>
2025-01-11 22:10:33 +0530

    Add: Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { Header, Body } from "./components/index-component/";
+import {
+  Header,
+  Restaurants,
+  RestaurantMenu,
+} from "./components/index-component/";
+import { Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-50">
       <Header />
-      <Body />
+      <Routes>
+        <Route path="/" element={<Restaurants />} />
+        <Route path="/menu/:menuId" element={<RestaurantMenu />} />
+      </Routes>
     </div>
   );
 };
