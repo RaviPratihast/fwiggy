da8979fb955efdd616975da90e49be01d8b6b342
Ravi Pratihast <ravipratihast@gmail.com>
2025-01-11 22:06:13 +0530

    Add: restaurant menu header refactor

--- src/components/RestaurantMenuHeader.jsx
+++ src/components/RestaurantMenuHeader.jsx
@@ -1,20 +1,43 @@
+import { TiStar } from "react-icons/ti";
 const RestaurantMenuHeader = ({ restaurantInfo }) => {
-  const { name, rating, totalRatings, priceForTwo, cuisines } = restaurantInfo;
-
+  const {
+    name,
+    rating,
+    totalRatings,
+    priceForTwo,
+    costForTwo,
+    cuisines,
+    avgRating,
+    totalRatingsString,
+    sla,
+  } = restaurantInfo;
+  console.log(restaurantInfo);
   return (
     <div className="bg-white rounded-lg shadow-md p-4 mt-20 ">
-      <h2 className="text-xl font-semibold mb-2">{name}</h2>
+      <h2 className="text-3xl font-bold mb-2">{name}</h2>
 
-      <div className="flex items-center gap-2 mb-2">
-        <span className="bg-green-100 text-green-800 px-2 py-1 rounded-md text-sm">
-          ★ {rating}
-        </span>
-        <span className="text-gray-600 text-sm">{totalRatings} ratings</span>
+      <div className="flex items-center gap-3">
+        <div className="flex items-center gap-1 mb-2">
+          <span className="text-lg text-green-600">
+            <TiStar />
+          </span>
+          <span className="text-sm font-bold">{avgRating}</span>
+
+          <span className="font-bold text-sm">({totalRatingsString})</span>
+        </div>
+
+        <p className=" font-bold mb-2">
+          ₹{priceForTwo || costForTwo / 100} for two
+        </p>
       </div>
-
-      <p className="text-gray-700 mb-2">{priceForTwo} for two</p>
-
-      <p className="text-gray-600 text-sm">Cuisines: {cuisines.join(", ")}</p>
+      <p className="text-orange-600 text-sm ml-1 underline font-bold">
+        {cuisines.join(", ")}
+      </p>
+      <div>
+        <span className="text-sm font-bold ml-1">
+          {sla.minDeliveryTime}-{sla.maxDeliveryTime} mins
+        </span>
+      </div>
     </div>
   );
 };
